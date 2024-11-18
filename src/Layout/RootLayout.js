3bf97878231a5466981664d4708b4fa94ac5027d
Titan antony <antonytitan96@gmail.com>
2024-11-18 20:46:09 +0300

    Chore add the navlinks so that they show on proper pages

--- src/Layout/RootLayout.js
+++ src/Layout/RootLayout.js
@@ -5,33 +5,56 @@ function RootLayout() {
   // Get the current location
   const location = useLocation();
 
-  // Determine the link text based on the current path
+  // Determine if on homepage or blog
   const isHomePage = location.pathname === '/'; // Default path for "My Website"
   const linkText = isHomePage ? 'My Blog' : 'My Website'; // Change based on path
-  const linkTo = isHomePage ? '/myBlog' : '/'; // Navigate to myBlog if on home page, else to home
+  const linkTo = isHomePage ? '/myBlog' : '/'; // Navigate to myBlog if on homepage, else to home
+
+  // Links for different navigation sections
+  const blogLinks = (
+    <div className='blog-nav'>
+      <NavLink to='/' className='nav-link-blog'>
+        Latest
+      </NavLink>
+      <NavLink to='/' className='nav-link-blog'>
+        React
+      </NavLink>
+      <NavLink to='/' className='nav-link-blog'>
+        AI
+      </NavLink>
+      <NavLink to='/' className='nav-link-blog'>
+        Flutter
+      </NavLink>
+      <NavLink to='/' className='nav-link-blog'>
+        My 30 Day Plan
+      </NavLink>
+    </div>
+  );
+
+  const websiteLinks = (
+    <div className='website-nav'>
+      <a href='#projects' className='nav-link-blog'>
+        Project
+      </a>
+      <NavLink to='#skills' className='nav-link-blog'>
+        Skills
+      </NavLink>
+      <NavLink to='#education' className='nav-link-blog'>
+        Education
+      </NavLink>
+      <NavLink to='#contact' className='nav-link-blog'>
+        Contact
+      </NavLink>
+    </div>
+  );
 
   return (
     <div className='rootLayout'>
       <nav className='nav'>
         <h1 className='logo'>Antony</h1>
 
-        <div className='blog-nav'>
-          <NavLink to='/' className='nav-link-blog'>
-            Latest
-          </NavLink>
-          <NavLink to='/' className='nav-link-blog'>
-            React
-          </NavLink>
-          <NavLink to='/' className='nav-link-blog'>
-            AI
-          </NavLink>
-          <NavLink to='/' className='nav-link-blog'>
-            Flutter
-          </NavLink>
-          <NavLink to='/' className='nav-link-blog'>
-            My 30 Day Plan
-          </NavLink>
-        </div>
+        {/* Conditional Rendering of Links */}
+        {isHomePage ? websiteLinks : blogLinks}
 
         {/* Render the link with dynamic text and correct navigation */}
         <NavLink to={linkTo} className='get-started-btn'>
