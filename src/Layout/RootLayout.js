0033266ce35cd79d9024d0bfe1d351bb042cd597
Titan antony <antonytitan96@gmail.com>
2025-01-20 18:51:37 +0300

    Chore: and a link to call a funtion to display the latest article

--- src/Layout/RootLayout.js
+++ src/Layout/RootLayout.js
@@ -1,69 +1,54 @@
+import React, { useState } from 'react';
 import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import './RootLayout.css';
 
 function RootLayout() {
-  // Get the current location
+  const [filterLatest, setFilterLatest] = useState(null);
   const location = useLocation();
 
-  // Determine if on homepage or blog
-  const isHomePage = location.pathname === '/'; // Default path for "My Website"
-  const linkText = isHomePage ? 'My Blog' : 'My Website'; // Change based on path
-  const linkTo = isHomePage ? '/myBlog' : '/'; // Navigate to myBlog if on homepage, else to home
+  const isHomePage = location.pathname === '/';
+  const linkText = isHomePage ? 'My Blog' : 'My Website';
+  const linkTo = isHomePage ? '/myBlog' : '/';
 
-  // Links for different navigation sections
   const blogLinks = (
-    <div className='blog-nav'>
-      <NavLink to='/' className='nav-link-blog'>
-        Latest
-      </NavLink>
-      <NavLink to='/' className='nav-link-blog'>
-        React
-      </NavLink>
-      <NavLink to='/' className='nav-link-blog'>
-        AI
-      </NavLink>
-      <NavLink to='/' className='nav-link-blog'>
-        Flutter
-      </NavLink>
-      <NavLink to='/' className='nav-link-blog'>
-        My 30 Day Plan
-      </NavLink>
+    <div className="blog-nav">
+      <nav className="navbar">
+        <ul>
+          <li onClick={() => filterLatest && filterLatest()}>Latest</li>
+        </ul>
+      </nav>
     </div>
   );
 
   const websiteLinks = (
-    <div className='website-nav'>
-      <a href='#projects' className='nav-link-blog'>
+    <div className="website-nav">
+      <a href="#projects" className="nav-link-blog">
         Project
       </a>
-      <NavLink to='#skills' className='nav-link-blog'>
+      <NavLink to="#skills" className="nav-link-blog">
         Skills
       </NavLink>
-      <NavLink to='#education' className='nav-link-blog'>
+      <NavLink to="#education" className="nav-link-blog">
         Education
       </NavLink>
-      <NavLink to='#contact' className='nav-link-blog'>
+      <NavLink to="#contact" className="nav-link-blog">
         Contact
       </NavLink>
     </div>
   );
 
   return (
-    <div className='rootLayout'>
-      <nav className='nav'>
-        <h1 className='logo'>Antony</h1>
-
-        {/* Conditional Rendering of Links */}
+    <div className="rootLayout">
+      <nav className="nav">
+        <h1 className="logo">Antony</h1>
         {isHomePage ? websiteLinks : blogLinks}
-
-        {/* Render the link with dynamic text and correct navigation */}
-        <NavLink to={linkTo} className='get-started-btn'>
+        <NavLink to={linkTo} className="get-started-btn">
           {linkText}
         </NavLink>
       </nav>
 
       <main>
-        <Outlet />
+        <Outlet context={{ setFilterLatest }} />
       </main>
     </div>
   );
